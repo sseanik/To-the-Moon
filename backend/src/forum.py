30d3e79cd17e92a919a962bb84d664c7866f120a
Karim <z5209523@ad.unsw.edu.au>
2021-03-07 15:13:28 +1100

    files for forum, portfolio, screener, stock, user and watchlist added to flask layout

---
+++ backend/src/forum.py
@@ -0,0 +1,19 @@
+####################
+#   Forum Module   #
+####################
+
+
+from flask import Blueprint
+
+FORUM_ROUTES = Blueprint('forum', __name__)
+
+
+###################################
+# Please leave all functions here #
+###################################
+
+
+
+################################
+# Please leave all routes here #
+################################
