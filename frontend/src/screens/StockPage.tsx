330d80b5b16454a5c0f71e78b287949657786ea6
pul-s4r <jjmoey@gmail.com>
2021-03-09 23:45:03 +1100

    Add chart options as state

--- frontend/src/screens/StockPage.tsx
+++ frontend/src/screens/StockPage.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Image } from "react-bootstrap";
 
 // import { render } from 'react-dom';
-import Highcharts from 'highcharts';
+import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 
 interface Props {
   // declare props types here
 }
 
-const options = {
+const test_options = {
   chart: {
     type: 'spline'
   },
@@ -24,6 +24,29 @@ const options = {
 };
 
 const StockPage: React.FC<Props> = () => {
+  const [options, setOptions] = useState({
+    title: {
+      text: '<Default Title>'
+    },
+    series: [
+      {
+        data: [
+          [1614004200000, 126],
+          [1614090600000, 125.86],
+          [1614177000000, 125.35],
+          [1614263400000, 120.99],
+          [1614349800000, 121.26],
+          [1614609000000, 127.79],
+          [1614695400000, 125.12],
+          [1614781800000, 122.06],
+          [1614868200000, 120.13],
+          [1614954600000, 121.42],
+          [1615213800000, 116.36]
+        ]
+      }
+    ]
+  });
+
   return (
     <Container>
       <Row className="justify-content-center">
@@ -35,7 +58,11 @@ const StockPage: React.FC<Props> = () => {
 
       <Row className="justify-content-center">
         <div>
-          <HighchartsReact highcharts={Highcharts} options={options} />
+          <HighchartsReact
+            highcharts={Highcharts}
+            constructorType={'stockChart'}
+            options={options}
+          />
         </div>
       </Row>
     </Container>
