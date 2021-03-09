34fbad149e3fbb172e3bc219878906a8cd78c62f
pul-s4r <jjmoey@gmail.com>
2021-03-09 23:21:56 +1100

    Add highchart react wrapper as dependency, sample graph

--- frontend/src/screens/StockPage.tsx
+++ frontend/src/screens/StockPage.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import { Container, Row, Image } from "react-bootstrap";
 
+// import { render } from 'react-dom';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
 interface Props {
   // declare props types here
 }
+
+const options = {
+  chart: {
+    type: 'spline'
+  },
+  title: {
+    text: 'My chart'
+  },
+  series: [
+    {
+      data: [1, 2, 1, 4, 3, 6]
+    }
+  ]
+};
 
 const StockPage: React.FC<Props> = () => {
   return (
@@ -13,6 +31,12 @@ const StockPage: React.FC<Props> = () => {
       </Row>
       <Row className="justify-content-center mt-2">
         <h1>Welcome To The Stock Page!</h1>
+      </Row>
+
+      <Row className="justify-content-center">
+        <div>
+          <HighchartsReact highcharts={Highcharts} options={options} />
+        </div>
       </Row>
     </Container>
   );
