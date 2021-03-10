6e71a94be2ef5bd4ed2701679dec3c904de366ec
pul-s4r <jjmoey@gmail.com>
2021-03-10 18:56:20 +1100

    Fetch data from backend

--- stock.py
+++ stock.py
@@ -26,11 +26,15 @@ def get_stock_value(symbol):
 
 def convert_to_opairs(df, label='4. close'):
     series = df[label].reset_index()
+    series['index'] = (pd.to_datetime(series['index']) \
+        - pd.Timestamp("1970-01-01")) // pd.Timedelta(milliseconds=1)
     return series.values.tolist()
 
 sample_df = get_stock_value("./demo/AMZN_daily_adjusted_20210305.json")
 sample_df = sample_df.reindex(index=sample_df.index[::-1])
-sample_data = convert_to_opairs(sample_df)
+sample_data_close = convert_to_opairs(sample_df, label="4. close")
+sample_data_high = convert_to_opairs(sample_df, label="2. high")
+sample_data_low = convert_to_opairs(sample_df, label="3. low")
 stock_name = "AMZN"
 
 
@@ -42,5 +46,9 @@ def get_stock_data():
     # an echo route just for testing
     return dumps({
         'name': stock_name,
-        'data': sample_data
+        'data': {
+            '4. close': sample_data_close,
+            '2. high': sample_data_high,
+            '3. low': sample_data_low
+        }
     })
