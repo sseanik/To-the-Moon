d75e0981c1598665599dc1b462450c06fc96c16c
Austin <A.landry@bigpond.com>
2021-03-10 19:36:08 +1100

    Notes

--- backend/src/database.py
+++ backend/src/database.py
@@ -14,7 +14,15 @@ PASS = os.getenv("DBPASS")
 def createDBConnection():
     try:
         conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASS)
+        cur = conn.cursor()
+        selectQuery = "SELECT 5;"
+        cur.execute(selectQuery)
+        query_results = cur.fetchall()
+        print(query_results)
         return conn
+
     except Exception as e:
         print("Database connection failed due to {}".format(e))
 
+
+createDBConnection()
