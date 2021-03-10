0e357abbf9805b7c95c4e98e208e7f72ad5b5d65
pul-s4r <jjmoey@gmail.com>
2021-03-10 22:18:37 +1100

    Add app routing for individual symbols

--- stock.py
+++ stock.py
@@ -3,10 +3,11 @@
 ####################
 
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 from json import dumps
 import pandas as pd
+import os
 
 from backend.src.AlphaVantageWrapper.AlphaVantageAPI import JSONLoader
 
@@ -21,21 +22,15 @@ def get_stock_value(symbol):
     In future this will be from a DB or the API, for now this loads from file
     so use the data's JSON filename instead
     """
-    stockdata = JSONLoader.load_json(symbol)
-    return pd.DataFrame.from_dict(stockdata, orient='index').astype('float')
+    stockdata, stockmetadata = JSONLoader.load_json(symbol)
+    return pd.DataFrame.from_dict(stockdata, orient='index').astype('float'), \
+        stockmetadata
 
 def convert_to_opairs(df, label='4. close'):
     series = df[label].reset_index()
     series['index'] = (pd.to_datetime(series['index']) \
         - pd.Timestamp("1970-01-01")) // pd.Timedelta(milliseconds=1)
     return series.values.tolist()
-
-sample_df = get_stock_value("./demo/AMZN_daily_adjusted_20210305.json")
-sample_df = sample_df.reindex(index=sample_df.index[::-1])
-sample_data_close = convert_to_opairs(sample_df, label="4. close")
-sample_data_high = convert_to_opairs(sample_df, label="2. high")
-sample_data_low = convert_to_opairs(sample_df, label="3. low")
-stock_name = "AMZN"
 
 
 ################################
@@ -44,11 +39,29 @@ stock_name = "AMZN"
 @STOCK_ROUTES.route('/stock', methods=['GET'])
 def get_stock_data():
     # an echo route just for testing
-    return dumps({
-        'name': stock_name,
-        'data': {
-            '4. close': sample_data_close,
-            '2. high': sample_data_high,
-            '3. low': sample_data_low
-        }
-    })
+    symbol = request.args.get('symbol')
+    data = {'name': "", 'data': ""}
+    filename = "demo/" + symbol + ".json" if symbol else ""
+    if symbol and os.path.isfile(filename):
+        sample_df, sample_metadata = get_stock_value(filename)
+        sample_df = sample_df.reindex(index=sample_df.index[::-1])
+        sample_data_close = convert_to_opairs(sample_df, label="4. close")
+        sample_data_high = convert_to_opairs(sample_df, label="2. high")
+        sample_data_low = convert_to_opairs(sample_df, label="3. low")
+        stock_name = sample_metadata['2. Symbol']
+
+        data = dumps({
+            'name': stock_name,
+            'data': {
+                '4. close': sample_data_close,
+                '2. high': sample_data_high,
+                '3. low': sample_data_low
+            }
+        })
+    else:
+        data = dumps({
+            'name': "",
+            'data': {},
+            'error': "Symbol not found"
+        })
+    return data
