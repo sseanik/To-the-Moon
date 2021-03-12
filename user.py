d0f49f0a3314097bd37fab00db208f7a7a7a627e
Karim Saad <karimsaad47@gmail.com>
2021-03-12 16:28:25 +1100

    register function optimised + login function started

--- user.py
+++ user.py
@@ -7,8 +7,14 @@ import bcrypt
 import jwt
 from json import dumps
 from flask import Blueprint, request
-from database import createDBConnection
+from database import USER, createDBConnection
 from dotenv import load_dotenv
+
+
+#######################
+# GLOBAL DECLARATIONS #
+#######################
+
 
 load_dotenv()
 JWT_SECRET = os.getenv("JWT_SECRET")
@@ -18,6 +24,7 @@ USER_ROUTES = Blueprint('user', __name__)
 ###################################
 # Please leave all functions here #
 ###################################
+
 
 def register_user(first_name, last_name, email, username, password):
     # open database connection
@@ -30,11 +37,11 @@ def register_user(first_name, last_name, email, username, password):
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     print(f'Encoded password: {hashed_password}')
 
-
     # check if user with current email already exists
     user_query = f"select id from users where email='{email}'"
     cur.execute(user_query, (email))
-    if (len(cur.fetchall()) != 0):
+    user_id = cur.fetchone()[0]
+    if not user_id:
         return {
             'status': 'failure',
             'error': 'There is already a user registered with this email'
@@ -43,11 +50,6 @@ def register_user(first_name, last_name, email, username, password):
     # insert user into database
     insert_query = "insert into Users (username, first_name, last_name, email, password) values (%s, %s, %s, %s, %s)"
     cur.execute(insert_query, (username, first_name, last_name, email, [hashed_password]))
-
-
-    # get that user's id
-    cur.execute(user_query, (email))
-    user_id = cur.fetchone()[0]
 
     # close database connection
     conn.commit()
@@ -61,12 +63,37 @@ def register_user(first_name, last_name, email, username, password):
     }
 
 
+def login_user(email, password):
+    # open database connection
+    conn = createDBConnection()
+    cur = conn.cursor()
+
+    # check if user with current email already exists
+    user_query = f"select id from users where email='{email}'"
+    cur.execute(user_query, (email))
+        
+
+    # close database connection
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+
 ################################
 # Please leave all routes here #
 ################################
+
 
 @USER_ROUTES.route('/register', methods=['POST'])
 def register_user_wrapper():
     data = request.get_json()
     result = register_user(data['first_name'], data['last_name'], data['email'], data['username'], data['password'])
+    return dumps(result)
+
+
+@USER_ROUTES.route('/login', methods=['POST'])
+def login_user_wrapper():
+    data = request.get_json()
+    result = login_user(data['email'], data['password'])
     return dumps(result)
