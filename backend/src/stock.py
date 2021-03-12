f2082a18e3ad247160ab97cc7c7fe9709360dc36
pul-s4r <jjmoey@gmail.com>
2021-03-12 23:11:13 +1100

    Add summary stats field

--- backend/src/stock.py
+++ backend/src/stock.py
@@ -23,14 +23,45 @@ def get_stock_value(symbol):
     so use the data's JSON filename instead
     """
     stockdata, stockmetadata = JSONLoader.load_json(symbol)
-    return pd.DataFrame.from_dict(stockdata, orient='index').astype('float'), \
-        stockmetadata
+    result = pd.DataFrame.from_dict(stockdata, orient='index').astype('float')
+    result = result.reindex(index=result.index[::-1])
+    result.index = pd.to_datetime(result.index)
+    return result, stockmetadata
 
 def convert_to_opairs(df, label='4. close'):
     series = df[label].reset_index()
     series['index'] = (pd.to_datetime(series['index']) \
         - pd.Timestamp("1970-01-01")) // pd.Timedelta(milliseconds=1)
     return series.values.tolist()
+
+def calculate_summary(df):
+    result = {}
+    if type(df) == pd.core.frame.DataFrame and df.shape[0] > 0:
+        result['previous_close'] = df.iloc[-2]['4. close'] \
+            if df.shape[0] >= 2 else 0
+        result['open'] = df.iloc[-1]['1. open'] # get this from the summary/quote endpoint
+        # result['bid'] = 0 # Not available
+        # result['ask'] = 0 # Not available
+        result['day_min'] = df.iloc[-1]['3. low']
+        result['day_max'] = df.iloc[-1]['2. high']
+        result['year_min'] = df.last("365D")['4. close'].min()
+        result['year_max'] = df.last("365D")['4. close'].max()
+        result['volume'] = df.iloc[-1]['6. volume']
+        result['average_volume'] = df.last("365D")['6. volume'].mean()
+    else:
+        result['previous_close'] = 0
+        result['open'] = 0 # get this from the summary/quote endpoint
+        # result['bid'] = 0 # Not available
+        # result['ask'] = 0 # Not available
+        result['day_min'] = 0
+        result['day_max'] = 0
+        result['year_min'] = 0
+        result['year_max'] = 0
+        result['volume'] = 0
+        result['average_volume'] = 0
+
+    return result
+
 
 
 ################################
@@ -44,7 +75,8 @@ def get_stock_data():
     filename = "demo/" + symbol + ".json" if symbol else ""
     if symbol and os.path.isfile(filename):
         sample_df, sample_metadata = get_stock_value(filename)
-        sample_df = sample_df.reindex(index=sample_df.index[::-1])
+        summary = calculate_summary(sample_df)
+        # sample_df = sample_df.reindex(index=sample_df.index[::-1])
         sample_data_close = convert_to_opairs(sample_df, label="4. close")
         sample_data_high = convert_to_opairs(sample_df, label="2. high")
         sample_data_low = convert_to_opairs(sample_df, label="3. low")
@@ -56,7 +88,8 @@ def get_stock_data():
                 '4. close': sample_data_close,
                 '2. high': sample_data_high,
                 '3. low': sample_data_low
-            }
+            },
+            'summary': summary
         })
     else:
         data = dumps({
--- frontend/src/screens/StockPage.tsx
+++ frontend/src/screens/StockPage.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Link, Route, useParams, useRouteMatch } from "react-router-dom";
-import { Container, Row, Image } from "react-bootstrap";
+import {  Container,
+          Row,
+          Image,
+          Table
+        } from "react-bootstrap";
 
 // import { render } from 'react-dom';
 import Highcharts from "highcharts/highstock";
@@ -10,6 +14,22 @@ import { getStockData } from "../redux/actions/userActions";
 
 interface Props {
   // declare props types here
+}
+
+interface graphOptionsT {
+  title: { text: string };
+  series: Array<{ name: string, data: Array<Array<number>> }>;
+}
+
+interface summaryDataT {
+    previous_close: number;
+    open: number;
+    day_min: number;
+    day_max: number;
+    year_min: number;
+    year_max: number;
+    volume: number;
+    average_volume: number;
 }
 
 const StockPage: React.FC<Props> = () => {
@@ -29,11 +49,11 @@ const StockPage: React.FC<Props> = () => {
 }
 
 const StockDisp: React.FC<Props> = (props) => {
-  var { symbol } = useParams();
+  var { symbol } = useParams<{ symbol: string }>();
 
-  const [graphOptions, setGraphOptions] = useState({
+  const [graphOptions, setGraphOptions] = useState<graphOptionsT | any>({
     title: {
-      text: "<Default Title>"
+      text: ""
     },
     series: [
       {
@@ -43,19 +63,32 @@ const StockDisp: React.FC<Props> = (props) => {
       }
     ]
   });
+  const [summaryData, setSummaryData] = useState<summaryDataT>({
+    previous_close: 0,
+    open: 0,
+    day_min: 0,
+    day_max: 0,
+    year_min: 0,
+    year_max: 0,
+    volume: 0,
+    average_volume: 0,
+  });
 
   useEffect(() => {
     async function fetchStock() {
       var stockdata = symbol ? await getStockData(symbol)
         : await getStockData("");
-      console.log(symbol);
-      console.log(Object.entries(stockdata.data));
+      // console.log(symbol);
+      // console.log(stockdata);
+      if (!stockdata) { return; }
       var seriesList = [];
 
       for (let [key, value] of Object.entries(stockdata.data)) {
         seriesList.push({name: key, data: value});
       }
       setGraphOptions({ ... graphOptions, series: seriesList });
+      setSummaryData(stockdata.summary);
+      console.log(stockdata.summary);
     }
 
     fetchStock();
@@ -78,6 +111,40 @@ const StockDisp: React.FC<Props> = (props) => {
             options={graphOptions}
           />
         </div>
+      </Row>
+
+      <Row className="justify-content-center mt-2">
+        <Table striped>
+          <tbody>
+            <tr>
+              <th>Previous Close</th>
+              <td>{summaryData.previous_close ? summaryData.previous_close : "N/A"}</td>
+            </tr>
+            <tr>
+              <th>Open</th>
+              <td>{summaryData.open ? summaryData.open : "N/A"}</td>
+            </tr>
+            <tr>
+              <th>Day Range</th>
+              <td>{summaryData.day_min ? summaryData.day_min : "N/A"}
+              - {summaryData.day_max ? summaryData.day_max : "N/A"}</td>
+            </tr>
+            <tr>
+              <th>52 Week Range</th>
+              <td>{summaryData.year_min ? summaryData.year_min : "N/A"}
+              - {summaryData.year_max ? summaryData.year_max : "N/A"}</td>
+            </tr>
+            <tr>
+              <th>Volume</th>
+              <td>{summaryData.volume ? summaryData.volume : "N/A"}</td>
+            </tr>
+            <tr>
+              <th>Average Volume</th>
+              <td>{summaryData.average_volume
+                  ? summaryData.average_volume.toFixed(0) : "N/A"}</td>
+            </tr>
+          </tbody>
+        </Table>
       </Row>
     </Container>
   );
