161d74b68a1472e9ac5c74b50043bc500a689efa
Karim Saad <karimsaad47@gmail.com>
2021-03-15 12:32:29 +1100

    portfolio page structure

--- frontend/src/screens/PortfolioPage.tsx
+++ frontend/src/screens/PortfolioPage.tsx
@@ -1,25 +1,49 @@
-import React from "react";
-import { Container, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Alert, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router";
 
 interface Props {
-  // declare
+  token?: string;
 }
 
 interface RouteMatchParams {
   name: string;
 }
 
-const PortfolioPage: React.FC<Props> = () => {
-  const { name } = useParams<RouteMatchParams>();
+const PortfolioPage: React.FC<Props> = (props) => {
+  const { token } = props;
+  const [authenticated, setAuthenticated] = useState(false);
 
-  // pass token to backend
+  useEffect(() => {
+    if (token !== "" || window.localStorage.getItem("Token")) {
+      setAuthenticated(true);
+    } else {
+      setAuthenticated(false);
+    }
+  }, [token]);
+
+  const { name } = useParams<RouteMatchParams>();
+  // get token from from redux store
   // retrieve userID
-  return (
-    <Container>
-      <Row className="justify-content-center">Hello {name}</Row>
-    </Container>
+  // pass token + userID to backend
+
+  console.log(name);
+  console.log(token);
+  console.log(authenticated);
+  const allowed = () => (
+    <Row>
+      <p>You have access to {name}</p>
+    </Row>
   );
+  const denied = () => (
+    <Row>
+      <Alert variant="danger">
+        <p>You do not have permission to access {name}</p>
+      </Alert>
+    </Row>
+  );
+
+  return <Container>{authenticated ? allowed() : denied()}</Container>;
 };
 
 export default PortfolioPage;
