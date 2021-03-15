5a06e9cb145c0480adee95b75872ecf0de0c8bf0
Austin <A.landry@bigpond.com>
2021-03-15 18:00:13 +1100

    Changed a variable

--- Tables/Holdings.sql
+++ Tables/Holdings.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS Holdings (
-    InvestmentID INT PRIMARY KEY,
-    UserID INT,
+    InvestmentID UUID NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
+    UserID TEXT,
     PortfolioName VARCHAR(30),
     PurchasePrice MONEY, 
     NumShares INT, 
     PurchaseDate DATE, 
-    DayChange decimal(5,2),
-    ChangeSinceInvestment decimal(5,2),
-    SecurityID INT
+    DayChange DECIMAL(10,2),
+    TotalChange DECIMAL(10,2),
+    StockTicker VARCHAR(10)
 );
