7c49773c382cd5cbfea5af42751115380c18d264
Austin Landry <austinlandry271@gmail.com>
2021-03-16 22:37:55 +1100

    adjusted schema

--- backend/src/Tables/Holdings.sql
+++ backend/src/Tables/Holdings.sql
@@ -1,11 +1,10 @@
 CREATE TABLE IF NOT EXISTS Holdings (
-    InvestmentID UUID NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
+    InvestmentID TEXT NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
     UserID TEXT,
     PortfolioName VARCHAR(30),
     PurchasePrice MONEY, 
     NumShares INT, 
     PurchaseDate DATE, 
-    DayChange DECIMAL(10,2),
     TotalChange DECIMAL(10,2),
     StockTicker VARCHAR(10)
 );
