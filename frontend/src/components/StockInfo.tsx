4e257599f22a5241f5f7041cd565525b4d8712ef
Karim Saad <karimsaad47@gmail.com>
2021-03-16 20:14:22 +1100

    extra info

--- frontend/src/components/StockInfo.tsx
+++ frontend/src/components/StockInfo.tsx
@@ -1,17 +1,39 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
 
 interface Props {
   stock_name: string;
   stock_price: string;
+  purchase_date: string;
+  purchase_price: string;
+  num_shares: string;
 }
 
 const StockInfo: React.FC<Props> = (props) => {
-  const { stock_name, stock_price } = props;
+  const {
+    stock_name,
+    stock_price,
+    purchase_date,
+    purchase_price,
+    num_shares,
+  } = props;
+
+  const handleDeleteStockClick = () => {
+    const deleteStock = async () => {};
+  };
+
   return (
-    <Row>
+    <Row className="border-bottom border-light my-2 w-100">
       <Col>{stock_name}</Col>
       <Col>{stock_price}</Col>
+      <Col>{purchase_date}</Col>
+      <Col>{purchase_price}</Col>
+      <Col>{num_shares}</Col>
+      <Col>
+        <Button variant="outline-danger" onClick={handleDeleteStockClick}>
+          Remove
+        </Button>
+      </Col>
     </Row>
   );
 };
