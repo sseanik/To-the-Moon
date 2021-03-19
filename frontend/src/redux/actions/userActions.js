c9efcf4e5f09ed5a934fd2cb654ea2f606b82621
Karim Saad <karimsaad47@gmail.com>
2021-03-20 09:43:20 +1100

    userActions login and register

---
+++ frontend/src/redux/actions/userActions.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import userConstants from "../constants/userConstants";
+
+export const register = (
+  first_name,
+  last_name,
+  email,
+  username,
+  password
+) => async (dispatch) => {
+  dispatch({
+    type: userConstants.REGISTER_USER_PENDING,
+    payload: { fisrt_name, last_name, email, username, password },
+  });
+  try {
+    const { data } = await axios.post("/register", {
+      first_name,
+      last_name,
+      email,
+      username,
+      password,
+    });
+    dispatch({
+      type: userConstants.REGISTER_USER_SUCCESS,
+      payload: data,
+    });
+    dispatch({
+      type: userConstants.LOGIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: userConstants.REGISTER_USER_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const login = (email, password) => async (dispatch) => {
+  dispatch({
+    type: userConstants.LOGIN_PENDING,
+    payload: { email, password },
+  });
+  try {
+    const { data } = await axios.post("/login", { email, password });
+    dispatch({
+      type: userConstants.LOGIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: userConstants.LOGIN_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- frontend/src/redux/reducers/userReducers.js
+++ frontend/src/redux/reducers/userReducers.js
@@ -4,7 +4,6 @@ export const registerUser = (
   state = {
     registerUser: {
       loading: false,
-      token: "",
       error: null,
     },
   },
