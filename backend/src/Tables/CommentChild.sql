33ccfa4391760e7cf44d9c94eec53fa65c185402
root <root@DESKTOP-GS02O6K.localdomain>
2021-03-26 19:17:11 +1100

    Fixed tables again

--- backend/src/Tables/CommentChild.sql
+++ backend/src/Tables/CommentChild.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS child_comment (
     child_id UUID NOT NULL DEFAULT UUID_GENERATE_V1() PRIMARY KEY,
-    parent_id TEXT NOT NULL FOREIGN KEY REFERENCES parent_comment(parent_id),
-    stock_ticker VARCHAR(10) FOREIGN KEY REFERENCES SecuritiesOverviews(StockTicker),
-    author_id UUID NOT NULL FOREIGN KEY REFERENCES Users(id),
-    is_verified FOREIGN KEY REFERENCES Users(is_verified),
+    parent_id TEXT NOT NULL REFERENCES parent_comment(parent_id),
+    stock_ticker VARCHAR(10) REFERENCES SecuritiesOverviews(StockTicker),
+    author_id UUID NOT NULL REFERENCES Users(id),
+    is_verified REFERENCES Users(is_verified),
     time_stamp TIMESTAMP NOT NULL,
     content TEXT,
     is_edited BOOLEAN DEFAULT FALSE,
--- backend/src/Tables/CommentParent.sql
+++ backend/src/Tables/CommentParent.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS parent_comment (
     parent_id UUID NOT NULL DEFAULT UUID_GENERATE_V1() PRIMARY KEY,
-    stock_ticker VARCHAR(10) FOREIGN KEY REFERENCES SecuritiesOverviews(StockTicker),
-    author_id UUID NOT NULL FOREIGN KEY REFERENCES Users(id),
-    is_verified FOREIGN KEY REFERENCES Users(is_verified),
+    stock_ticker VARCHAR(10) REFERENCES SecuritiesOverviews(StockTicker),
+    author_id UUID NOT NULL REFERENCES Users(id),
+    is_verified REFERENCES Users(is_verified),
     time_stamp TIMESTAMP NOT NULL,
     content TEXT,
     is_edited BOOLEAN DEFAULT FALSE,
