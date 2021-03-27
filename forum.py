94997fefe60d8e81fb2718d0a31e1ff8c52a27ff
Sean Smith <seaniksmith@gmail.com>
2021-03-27 15:07:57 +1100

    Added functionality for submitting a comment or a reply

--- forum.py
+++ forum.py
@@ -48,7 +48,8 @@ def post_comment(user_id, stock_ticker, timestamp, content, parent_id=None):
     if not validate_timestamp(timestamp):
         return {
             'status': 400,
-            'message': 'Timestamp provided is invalid'
+            'message': 'Timestamp provided is invalid',
+            'content': ""
         }
 
     # Censor rude words
@@ -58,13 +59,13 @@ def post_comment(user_id, stock_ticker, timestamp, content, parent_id=None):
     conn = createDBConnection()
     cur = conn.cursor()
 
-    # If no parent_id is provided, comment is a parent comment
+    # If no parent_id is provided, comment is a parent comment (comment)
     if not parent_id:
-        insert_query = "INSERT INTO parentcomment (stock_ticker, author_id, time_stamp, content) VALUES (%s, %s, %s, %s)"
+        insert_query = "INSERT INTO ForumComment (stock_ticker, author_id, time_stamp, content) VALUES (%s, %s, %s, %s)"
         values = (stock_ticker, user_id, timestamp, content)
-    # Otherwise, using the provided parent id, it is a child comment
+    # Otherwise, using the provided parent id, it is a child comment (reply)
     else:
-        insert_query = "INSERT INTO childcomment (parent_id, stock_ticker, author_id, time_stamp, content) VALUES (%s, %s, %s, %s, %s)"
+        insert_query = "INSERT INTO ForumReply (comment_id, stock_ticker, author_id, time_stamp, content) VALUES (%s, %s, %s, %s, %s)"
         values = (parent_id, stock_ticker, user_id, timestamp, content)
 
     # Attempt to insert values into the DB, handling invalid Data cases in the insert
@@ -75,6 +76,7 @@ def post_comment(user_id, stock_ticker, timestamp, content, parent_id=None):
     except:
         status = 400
         message = "Invalid data was provided to the Database"
+        content = ""
 
     conn.commit()
     cur.close()
@@ -127,6 +129,10 @@ def submit_reply():
     return dumps(result)
 
 
-if __name__ == "__main__":
-    print(post_comment("0ee69cfc-83ce-11eb-8620-0a4e2d6dea13", "IBM", 1616810169114,
-          "fuck tesla. Okay how are you doing?", "2f477d66-8e93-11eb-b5ed-0a4e2d6dea13"))
+# if __name__ == "__main__":
+#     # Testing Posting Parent Comment
+#     print(post_comment("0ee69cfc-83ce-11eb-8620-0a4e2d6dea13", "IBM", 1616810169114,
+#           "IBM is great. Microsoft's acquisition of Discord is shit"))
+#     # Testing Posting Child Comment
+#     print(post_comment("0ee69cfc-83ce-11eb-8620-0a4e2d6dea13", "IBM", 1616810169114,
+#           "Fuck IBM. Move along, don't buy", "562c5a86-8ea8-11eb-bf93-0a4e2d6dea13"))
