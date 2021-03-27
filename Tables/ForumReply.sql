513184073619d383040c5f638e830d0266ccf6d2
Sean Smith <seaniksmith@gmail.com>
2021-03-27 13:47:34 +1100

    Updated forum tables

---
+++ Tables/ForumReply.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS ForumReply (
+    reply_id UUID NOT NULL DEFAULT UUID_GENERATE_V1() PRIMARY KEY,
+    comment_id UUID REFERENCES ForumComment(comment_id),
+    stock_ticker VARCHAR(10) REFERENCES SecuritiesOverviews(StockTicker),
+    author_id UUID REFERENCES Users(id),
+    time_stamp BIGINT NOT NULL,
+    content VARCHAR(5000),
+    upvote_user_ids UUID [],
+    downvote_user_ids UUID [],
+    is_edited BOOLEAN DEFAULT FALSE
+);
