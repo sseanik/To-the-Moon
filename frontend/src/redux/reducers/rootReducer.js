b0bf7224756899cf1b8c97fa3eed740ad81c30d3
Karim Saad <karimsaad47@gmail.com>
2021-03-29 10:08:15 +1100

    old files removed

--- frontend/src/redux/reducers/rootReducer.js
+++ frontend/src/redux/reducers/rootReducer.js
@@ -1,14 +1,10 @@
 import { combineReducers } from "redux";
 import { landingNewsReducer, stockNewsReducer } from "./newsReducer";
 import userReducer from "../reducers/userReducer";
-import createStock from "./createStock";
 import portfolioReducer from "./portfolioReducer";
-import submitCreateStockForm from "./submitCreateStockForm";
 export default combineReducers({
   userReducer,
   portfolioReducer,
-  submitCreateStockForm,
-  createStock,
   landingNewsReducer,
   stockNewsReducer,
 });
