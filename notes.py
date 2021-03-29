3a9923c05a45e3dd77f5c2f8638af6a483387296
Austin Landry <austinlandry271@gmail.com>
2021-03-29 22:56:23 +1100

    resolved PR comments

--- notes.py
+++ notes.py
@@ -18,19 +18,18 @@ NOTE_ROUTES = Blueprint('notes', __name__)
 # Please leave all functions here #
 ###################################
 
-# TEST THIS
 def addNote(userID, title, content, stock_symbols, portfolio_names, external_references, internal_references):
-    if len(title) >= 300:
-        return {'status': 400, 'message': 'The note title cannot be more than 300 characters. Try a new title.'}
+    if len(title) >= 300 or len(title) == 0:
+        return {'status': 400, 'message': 'The note title must be at least 1 character and no more than 300 characters. Try a new title.'}
 
     if len(content) >= 5000:
-        return {'status': 400, 'message': 'The note contnets cannot be more than 5000 characters. Please reduct content or create a new note.'}
+        return {'status': 400, 'message': 'The note contents cannot be more than 5000 characters. Please reduct content or create a new note.'}
 
     conn = createDBConnection()
     cur = conn.cursor()
     sqlQuery = """
-    insert into Notes (userID, title, content, stock_symbols, portfolio_names, external_references, internal_references) 
-    values (%s, %s, %s, %s::TEXT[], %s::TEXT[], %s::TEXT[], %s::TEXT[])
+    INSERT INTO notes (userID, title, content, stock_symbols, portfolio_names, external_references, internal_references) 
+    VALUES (%s, %s, %s, %s::TEXT[], %s::TEXT[], %s::TEXT[], %s::TEXT[])
     """
     try:
         cur.execute(sqlQuery, (userID, title, content, stock_symbols, portfolio_names, external_references, internal_references))
@@ -42,30 +41,29 @@ def addNote(userID, title, content, stock_symbols, portfolio_names, external_ref
     except psycopg2.errors.UniqueViolation:
         response = {
             'status' : 400,
-            'message' : 'There is already a note called \'' + title + '\'. Try anther title name.'
+            'message' : 'There is already a note called \'' + title + '\'. Try another title name.'
         }
     except:
         response = {
             'status' : 400,
             'message' : 'Something went wrong when inserting'
-        }
+        } 
     conn.close()
     return response
 
 
-# TEST THIS
 def editNote(userID, old_title, new_title, content, stock_symbols, portfolio_names, external_references, internal_references):
-    if len(new_title) >= 300:
-        return {'status': 400, 'message': 'The note title cannot be more than 300 characters. Try a new title.'}
+    if len(title) >= 300 or len(title) == 0:
+        return {'status': 400, 'message': 'The note title must be at least 1 character and no more than 300 characters. Try a new title.'}
 
     if len(content) >= 5000:
-        return {'status': 400, 'message': 'The note contnets cannot be more than 5000 characters. Please reduct content or create a new note.'}
+        return {'status': 400, 'message': 'The note contents cannot be more than 5000 characters. Please reduct content or create a new note.'}
 
     conn = createDBConnection()
     cur = conn.cursor()
     sqlQuery = """
-    update notes set title=%s, content=%s, stock_symbols=%s::TEXT[], portfolio_names=%s::TEXT[], external_references=%s::TEXT[], internal_references=%s::TEXT[]
-    where userID=%s and title=%s
+        UPDATE notes SET title=%s, content=%s, stock_symbols=%s::TEXT[], portfolio_names=%s::TEXT[], external_references=%s::TEXT[], internal_references=%s::TEXT[]
+        WHERE userID=%s AND title=%s
     """
     try:
         cur.execute(sqlQuery, (new_title, content, stock_symbols, portfolio_names, external_references, internal_references, userID, old_title))
@@ -77,7 +75,7 @@ def editNote(userID, old_title, new_title, content, stock_symbols, portfolio_nam
     except psycopg2.errors.UniqueViolation:
         response = {
             'status' : 400,
-            'message' : 'There is already a note called \'' + new_title + '\'. Try anther title name.'
+            'message' : 'There is already a note called \'' + new_title + '\'. Try another title name.'
         }
     except:
         response = {
@@ -88,11 +86,10 @@ def editNote(userID, old_title, new_title, content, stock_symbols, portfolio_nam
     return response
 
 
-# TEST THIS
 def deleteNote(userID, title):
     conn = createDBConnection()
     cur = conn.cursor()
-    sqlQuery = "delete from notes where userID=%s and title=%s"
+    sqlQuery = "DELETE FROM notes WHERE userID=%s AND title=%s"
     cur.execute(sqlQuery, (userID, title))
     conn.commit()
     conn.close()
@@ -102,7 +99,7 @@ def deleteNote(userID, title):
 def getNote(userID, title):
     conn = createDBConnection()
     cur = conn.cursor()
-    sqlQuery = "select * from notes where userID=%s and title=%s"
+    sqlQuery = "SELECT * FROM notes WHERE userID=%s AND title=%s"
     cur.execute(sqlQuery, (userID, title))
     queryResults = cur.fetchall()[0]
     note = {
@@ -120,7 +117,7 @@ def getNote(userID, title):
 def getRelevantNotes(userID, stock_symbols, portfolio_names):
     conn = createDBConnection()
     cur = conn.cursor()
-    sqlQuery = "select * from notes where userID=%s and (%s::TEXT[] && stock_symbols or %s::TEXT[] && portfolio_names)"
+    sqlQuery = "SELECT * FROM notes WHERE userID=%s AND (%s::TEXT[] && stock_symbols OR %s::TEXT[] && portfolio_names)"
     cur.execute(sqlQuery, (userID, stock_symbols, portfolio_names))
     queryResults = cur.fetchall()
     notes = []
@@ -141,7 +138,7 @@ def getRelevantNotes(userID, stock_symbols, portfolio_names):
 def getAllNotes(userID):
     conn = createDBConnection()
     cur = conn.cursor()
-    sqlQuery = "select * from notes where userID=%s"
+    sqlQuery = "SELECT * FROM notes WHERE userID=%s"
     cur.execute(sqlQuery, (userID, ))
     queryResults = cur.fetchall()
     notes = []
@@ -163,7 +160,6 @@ def getAllNotes(userID):
 # Please leave all routes here #
 ################################
 
-# TEST THIS
 @NOTE_ROUTES.route('notes/addNote', methods=['POST'])
 def createUsersNote():
     token = request.headers.get('Authorization')
@@ -178,7 +174,6 @@ def createUsersNote():
     return dumps(response)
 
 
-# TEST THIS
 @NOTE_ROUTES.route('notes/editNote', methods=['PUT'])
 def editUsersNote():
     token = request.headers.get('Authorization')
@@ -194,7 +189,6 @@ def editUsersNote():
     return dumps(response)
 
 
-# TEST THIS
 @NOTE_ROUTES.route('/notes/deleteNote', methods=['DELETE'])
 def deleteUsersNote():
     token = request.headers.get('Authorization')
@@ -204,7 +198,6 @@ def deleteUsersNote():
     return dumps(response)
 
 
-# TEST THIS
 @NOTE_ROUTES.route('/notes/getNotes', methods=['GET'])
 def getUsersNotes():
     token = request.headers.get('Authorization')
