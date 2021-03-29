fee3e61ca02c8b5f4b19b80266f2b2f933099b93
Austin Landry <austinlandry271@gmail.com>
2021-03-29 14:30:41 +1100

    adjusted datatypes and renamed sql file

--- backend/src/Tables/portfolios.sql
+++ backend/src/Tables/portfolios.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS Portfolios (
+CREATE TABLE IF NOT EXISTS portfolios (
     portfolio_name VARCHAR(30),
-    user_id TEXT,
+    user_id uuid,
     PRIMARY KEY(portfolio_name, user_id)
 );
