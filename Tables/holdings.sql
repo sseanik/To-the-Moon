98e5f5d582decb4bd2d718ecb7ac5bf89ed51ef4
Austin Landry <austinlandry271@gmail.com>
2021-03-29 14:30:41 +1100

    adjusted datatypes and renamed sql file

--- Tables/holdings.sql
+++ Tables/holdings.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS Holdings (
+CREATE TABLE IF NOT EXISTS holdings (
     investment_id TEXT NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
-    user_id TEXT,
+    user_id uuid,
     portfolio_name VARCHAR(30),
-    purchase_price MONEY,
-    num_shares INT,
+    purchase_price DOUBLE PRECISION,
+    num_shares BIGINT,
     purchase_date DATE,
     stock_ticker VARCHAR(10)
 );
