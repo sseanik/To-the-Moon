1bc7a96c378b3e464d64e7cb1d68ffe1455cde51
Austin Landry <austinlandry271@gmail.com>
2021-04-06 22:00:26 +1000

    screener stub

--- screener.py
+++ screener.py
@@ -12,7 +12,14 @@ SCREENER_ROUTES = Blueprint('screener', __name__)
 # Please leave all functions here #
 ###################################
 
+def screen_stocks():
+    """
+    args:
+        
 
+    Returns:
+        dict: Status code, message, list of stock IDs
+    """
 
 ################################
 # Please leave all routes here #
