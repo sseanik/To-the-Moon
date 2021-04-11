b26783e27bdc345d051d960ab93de8038253d306
Austin Landry <austinlandry271@gmail.com>
2021-04-11 21:50:53 +1000

    screener work

--- backend/src/screener.py
+++ backend/src/screener.py
@@ -28,6 +28,14 @@ def screener_save(screener_name, user_id, parameters):
             'status' : 200,
             'error' : 'Parameters must be a dictionary.'
         }
+        return rtrn
+
+    if len(screener_name) > 30:
+        rtrn = {
+            'status' : 200,
+            'error' : 'Screener name cannot be more than 30 characters. Try a new name.'
+        }
+        return rtrn
 
     conn = create_DB_connection()
     cur = conn.cursor()
@@ -81,15 +89,46 @@ def screener_load(screener_name, user_id):
     return rtrn
 
 
-def screener_edit_parameters():
-    pass
+def screener_delete(screener_name, user_id):
+    conn = create_DB_connection()
+    cur = conn.cursor()
+    sql_query = "DELETE FROM screeners WHERE screener_name=%s AND user_id=%s"
+    cur.execute(sql_query, (screener_name, user_id))
+    conn.commit()
+    conn.close()
+    return {'status' : 200, 'message' : "Screen called \'" + screener_name + "\' have been removed."}
+
+
+# Not being used?
+def screener_edit_parameters(screener_name, user_id, parameters):
+    if not isinstance(parameters, dict):
+        rtrn = {
+            'status' : 200,
+            'error' : 'Parameters must be a dictionary.'
+        }
+        return rtrn
+
+    conn = create_DB_connection()
+    cur = conn.cursor()
+    sql_query = "UPDATE screeners SET parameters=%s WHERE screener_name=%s AND user_id=%s"
+    try:
+        cur.execute(sql_query, (str(parameters), screener_name, user_id))
+        rtrn = {
+            'status' : 200, 
+            'message' :  'Screen called \'' + screener_name + '\' has been updated.'
+        }
+    except:
+        rtrn = {
+            'status' : 400,
+            'error' : 'Something went wrong when updating screeners.'
+        }
+    conn.commit()
+    conn.close()
+    return rtrn
+
 
 def screener_edit_name():
     pass
-
-def screener_delete():
-    pass
-
 
 ################################
 # Please leave all routes here #
@@ -115,6 +154,19 @@ test = {
 
     }
 }
-print(screener_save("new test screener", "02708412-912d-11eb-a6dc-0a4e2d6dea13", test))
-
+#print(screener_save("new test screener", "02708412-912d-11eb-a6dc-0a4e2d6dea13", test))
+#print(screener_save("test screener", "02708412-912d-11eb-a6dc-0a4e2d6dea13", test))
 #print(screener_load("test screener", "02708412-912d-11eb-a6dc-0a4e2d6dea13"))
+#print(screener_delete("new test screener", "02708412-912d-11eb-a6dc-0a4e2d6dea13"))
+test2 = {
+    'securities_overviews' : {
+        'eps' : (4, 8),
+        'beta' : (1, None),
+        'payout_ratio' : (None, 0.3)
+    }    
+}
+print(screener_edit_parameters("test screener", "02708412-912d-11eb-a6dc-0a4e2d6dea13", test2))
+
+
+
+
