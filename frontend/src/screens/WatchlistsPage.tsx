962763ce2c3109894f5c20cfc9d21615d56a7976
Karim Saad <karimsaad47@gmail.com>
2021-04-12 13:07:17 +1000

    following toggle

--- frontend/src/screens/WatchlistsPage.tsx
+++ frontend/src/screens/WatchlistsPage.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import { Button, Container } from "react-bootstrap";
+
 const WatchlistsPage: React.FC = () => {
-  return <h1>Watchlists Page</h1>;
+  const [following, setFollowing] = useState(false);
+  return (
+    <Container fluid>
+      {following ? (
+        <Button
+          onClick={() => {
+            setFollowing(false);
+          }}
+        >
+          View All
+        </Button>
+      ) : (
+        <Button
+          onClick={() => {
+            setFollowing(true);
+          }}
+        >
+          View Following
+        </Button>
+      )}
+      {following ? (
+        <p>Displaying only followed watchlists</p>
+      ) : (
+        <p>Displaying all watchlists</p>
+      )}
+    </Container>
+  );
 };
 
 export default WatchlistsPage;
