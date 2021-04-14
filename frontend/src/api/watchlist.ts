0db9eff9ece157215003110c0011c45415d82b63
Karim Saad <56572545+karim-saad@users.noreply.github.com>
2021-04-14 19:08:47 +1000

    Watchlists (#78)
    * base for new pages
    
    * header link amended
    
    * watchlist identifier
    
    * following toggle
    
    * initial redux constants and reducers
    
    * adjust typing
    
    * bugfix
    
    * delete watchlists
    
    * tabs for watchlist page
    
    * watchlist info view
    
    * watchlist info displayed
    
    * base API + actions
    
    * get following
    
    * add watchlists template
    
    * all base functionality
    
    * proxy page until parameters are defined
    
    * added watchlist to root
    
    * typing
    
    * first untested edition of watchlist functions
    
    * watchlist schema's
    
    * refactor
    
    * hotfixes to backend
    
    * data interaction implemented
    
    * loading
    
    * hotfixes + author
    
    * typing fix
    
    * publish portfolio
    
    * individual watchlist page id extract
    
    * added get individual watchlist functionality with route
    
    * watchlist backend fixes
    
    * endpoint adjustments
    
    * watchlist stock info display component
    
    * get singular watchlist redux
    
    * individual watchlist page
    
    * commit database
    
    * redux following changes
    
    * follow button changes
    
    * appropriate method
    
    * delete watchlist
    
    * more bugfixes
    
    * changes to reference
    
    Co-authored-by: Austin Landry <austinlandry271@gmail.com>

---
+++ frontend/src/api/watchlist.ts
@@ -0,0 +1,101 @@
+import * as config from "../config.json";
+import Utils from "./utils";
+
+const url = `http://localhost:${config.BACKEND_PORT}`;
+
+interface Header {
+  "Content-Type": string;
+  Authorization: string | null;
+}
+
+interface Options {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  headers: Header;
+  body?: string;
+}
+
+const watchlistAPI = {
+  getWatchlists: () => {
+    const endpoint = "/watchlist/get_all";
+    const options: Options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Utils.getToken(),
+      },
+    };
+    return Utils.getJSON(`${url}${endpoint}`, options);
+  },
+  getWatchlist: (watchlist_id: string) => {
+    const endpoint = `/watchlist/get_watchlist?watchlist_id=${watchlist_id}`;
+    const options: Options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Utils.getToken(),
+      },
+    };
+    return Utils.getJSON(`${url}${endpoint}`, options);
+  },
+  addWatchlist: (portfolio_name: string, description: string) => {
+    const endpoint = "/watchlist";
+    const options: Options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Utils.getToken(),
+      },
+      body: JSON.stringify({ portfolio_name, description }),
+    };
+    return Utils.getJSON(`${url}${endpoint}`, options);
+  },
+  deleteWatchlist: (watchlist_id: string) => {
+    const endpoint = "/watchlist";
+    const options: Options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Utils.getToken(),
+      },
+      body: JSON.stringify({ watchlist_id }),
+    };
+    return Utils.getJSON(`${url}${endpoint}`, options);
+  },
+  getFollowing: () => {
+    const endpoint = "/watchlist/userslist";
+    const options: Options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Utils.getToken(),
+      },
+    };
+    return Utils.getJSON(`${url}${endpoint}`, options);
+  },
+  addFollowing: (watchlist_id: string) => {
+    const endpoint = "/watchlist/subscribe";
+    const options: Options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Utils.getToken(),
+      },
+      body: JSON.stringify({ watchlist_id }),
+    };
+    return Utils.getJSON(`${url}${endpoint}`, options);
+  },
+  deleteFollowing: (watchlist_id: string) => {
+    const endpoint = "/watchlist/subscribe";
+    const options: Options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Utils.getToken(),
+      },
+      body: JSON.stringify({ watchlist_id }),
+    };
+    return Utils.getJSON(`${url}${endpoint}`, options);
+  },
+};
+
+export default watchlistAPI;
