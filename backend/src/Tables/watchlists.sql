a95a1303f1e80c2e5a285b6bf24a6d50a4f4f222
Austin Landry <austinlandry271@gmail.com>
2021-04-14 18:45:16 +1000

    resolved schema issues

--- backend/src/Tables/watchlists.sql
+++ backend/src/Tables/watchlists.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS watchlists (
-    watchlist_id TEXT NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
-    user_id UUID,
+    watchlist_id UUID NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
+    user_id UUID REFERENCES Users(id),
     watchlist_name VARCHAR(30),
     watchlist_description TEXT,
     stock_tickers VARCHAR(10) [] DEFAULT ARRAY []::VARCHAR(10) [],
