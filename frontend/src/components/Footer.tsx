453e8bbd6cf9db16ae72e7f3b36cf1d912c677d3
Karim Saad <56572545+karim-saad@users.noreply.github.com>
2021-04-20 21:27:39 +1000

    Karim final touches (#95)
    * noteConstants js > ts
    
    * api js ? ts
    
    * forumactions js > ts
    
    * investmentactions js > ts
    
    * newsactions js > ts
    
    * note + trend actions js > ts
    
    * portfolio stock user actions js > ts
    
    * general types
    
    * forum reducer + action changes
    
    * forum reducer
    
    * user reducer with changes from null > empty string
    
    * eye instead of signin
    
    * investment reducer js > ts + slight refactor
    
    * news js > ts
    
    * note js > ts
    
    * trend js > ts
    
    * configurestore js > ts
    
    * portfolio js > ts + slight refactor
    
    * stock js > ts
    
    * edit instead of eye
    
    * deps array change
    
    * deps
    
    * create form still with errors
    
    * instant delete changes
    
    * create instant change
    
    * size up
    
    * revised investment model
    
    * refresh portfolios
    
    * comment/reply detail
    
    * reducer change
    
    * screener api js > ts
    
    * actions js > ts
    
    * types params
    
    * range select to arrow function + ts
    
    * watchlist info cards to match portfolio info cards
    
    * basic footer
    
    * footer content
    
    * footer style + clean app.css
    
    * dark blue container
    
    * little shadow for footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Row } from "react-bootstrap";
 
 const Footer: React.FC = () => {
   return (
-    <Row className="bg-dark fixed-bottom justify-content-center py-1">
+    <Row className="bg-dark fixed-bottom justify-content-center py-1 footer">
       <p className="text-light my-1">
         © 2021 To The Moon | All Rights Reserved
       </p>
