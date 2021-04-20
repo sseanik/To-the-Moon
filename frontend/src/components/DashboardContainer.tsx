667cf701d74d5b5d55493cbb2c617776afe7830b
Sarah Tan <sarah.tan1@student.unsw.edu.au>
2021-04-20 16:21:59 +1000

    DashboardContainer

--- frontend/src/components/DashboardContainer.tsx
+++ frontend/src/components/DashboardContainer.tsx
@@ -1,5 +1,60 @@
-const DashboardContainer: React.FC = () => {
-  return <></>;
+import { useEffect } from "react";
+import { Container, Alert } from "react-bootstrap";
+import { connect } from "react-redux";
+import ClipLoader from "react-spinners/ClipLoader";
+import { BlocksContainer } from "../components";
+import dashboardActions from "../redux/actions/dashboardActions";
+
+interface StateProps {
+  dashboardId: string;
+  loading: boolean;
+  error: string;
 }
 
-export default DashboardContainer;
+interface DispatchProps {
+  getBlocks: (payload: GetBlocksParams) => void;
+}
+
+interface GetBlocksParams {
+  dashboardId: string;
+}
+
+const DashboardContainer: React.FC<StateProps & DispatchProps> = (props) => {
+  const { dashboardId, loading, error, getBlocks } = props;
+
+  useEffect(() => {
+    if (dashboardId) {
+      getBlocks({ dashboardId });
+    }
+  }, [dashboardId, getBlocks]);
+
+  const loadingSpinnerComponent = (
+    <div>
+      <ClipLoader color={"green"} loading={loading}></ClipLoader>
+      <h5>Gathering blocks...</h5>
+    </div>
+  );
+
+  return (
+    <Container fluid>
+      {error ? <Alert variant="danger">{error}</Alert> : null}
+      {loading ? loadingSpinnerComponent : null}
+      <BlocksContainer />
+    </Container>
+  );
+};
+
+const mapStateToProps = (state: any) => ({
+  dashboardId: state.dashboardReducer.dashboardId,
+  loading: state.dashboardReducer.getBlocks.loading,
+  error: state.dashboardReducer.getBlocks.error,
+});
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    getBlocks: (payload: GetBlocksParams) =>
+      dispatch(dashboardActions.getBlocks(payload)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardContainer);
