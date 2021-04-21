53cb65cabb748b9da4e3764895df41ce9a5d37e6
Sarah Tan <sarah.tan1@student.unsw.edu.au>
2021-04-21 15:21:42 +1000

    News and stock block tables

--- Tables/dashboard_blocks.sql
+++ Tables/dashboard_blocks.sql
@@ -7,3 +7,11 @@ CREATE TABLE IF NOT EXISTS portfolio_block (
     portfolio_name VARCHAR(30),
     detailed BOOLEAN default false
 ) INHERITS (dashboard_blocks);
+
+CREATE TABLE IF NOT EXISTS news_block (
+    stock_ticker VARCHAR(10)
+) INHERITS (dashboard_blocks);
+
+CREATE TABLE IF NOT EXISTS stock_block (
+    stock_ticker VARCHAR(10)
+) INHERITS (dashboard_blocks);
