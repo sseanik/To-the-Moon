aab73eec2d2bfcc7bec38ebe00adf13fd30a593e
Sean Smith <seaniksmith@gmail.com>
2021-09-03 00:34:42 +1000

    Updated models

--- backend/src/AlphaVantageWrapper/AlphaVantageAPI.py
+++ backend/src/AlphaVantageWrapper/AlphaVantageAPI.py
@@ -4,6 +4,7 @@ import csv
 import requests
 from datetime import date
 
+
 class AlphaVantageAPI:
     """
     Wrapper to construct AlphaVantage URLs, send HTTP queries and receive HTTP
@@ -12,16 +13,21 @@ class AlphaVantageAPI:
     Docs: https://www.alphavantage.co/documentation/
     TODO: Store and load API key from ENV file
     """
+
     base_url = "https://www.alphavantage.co/query?"
-    api_key = "1PRBO66RYM7SV7B9"
+    api_key = "7BTAD2U79MPR388H"
 
     @classmethod
     def _construct_url(cls, *args, **kwargs):
-        param_string = ''
+        param_string = ""
         for k in kwargs:
-            param_string += '&{}={}'.format(k, kwargs[k])
-        return cls.base_url + 'function={}'.format(args[0]) \
-            + param_string + '&apikey={}'.format(cls.api_key)
+            param_string += "&{}={}".format(k, kwargs[k])
+        return (
+            cls.base_url
+            + "function={}".format(args[0])
+            + param_string
+            + "&apikey={}".format(cls.api_key)
+        )
 
     @staticmethod
     def _get_query(url, proxies={}, headers={}):
@@ -30,12 +36,14 @@ class AlphaVantageAPI:
 
     @staticmethod
     def save_json(company_name, data, label="generic"):
-        with open(f'./{company_name}_{label}_{date.today().strftime("%Y%m%d")}.json', 'w') as outfile:
+        with open(
+            f'./{company_name}_{label}_{date.today().strftime("%Y%m%d")}.json', "w"
+        ) as outfile:
             json.dump(data, outfile)
 
     @staticmethod
     def load_json(filename):
-        with open(filename, 'r') as infile:
+        with open(filename, "r") as infile:
             data = json.load(infile)
             return data
 
@@ -46,41 +54,41 @@ class TimeSeries:
     """
     Wrapper for time series endpoints
     """
+
     @staticmethod
     def _construct_intraday(symbol, interval, **kwargs):
-        return AlphaVantageAPI._construct_url("TIME_SERIES_INTRADAY", \
-            symbol=symbol, interval=interval, **kwargs)
+        return AlphaVantageAPI._construct_url(
+            "TIME_SERIES_INTRADAY", symbol=symbol, interval=interval, **kwargs
+        )
 
     @staticmethod
     def _construct_daily_adjusted(symbol, **kwargs):
-        return AlphaVantageAPI._construct_url("TIME_SERIES_DAILY_ADJUSTED", \
-            symbol=symbol, **kwargs)
-    
+        return AlphaVantageAPI._construct_url(
+            "TIME_SERIES_DAILY_ADJUSTED", symbol=symbol, **kwargs
+        )
+
     @staticmethod
     def _construct_company_overview(symbol, **kwargs):
-        return AlphaVantageAPI._construct_url("OVERVIEW", \
-            symbol=symbol, **kwargs)
+        return AlphaVantageAPI._construct_url("OVERVIEW", symbol=symbol, **kwargs)
 
     @staticmethod
     def _construct_income_statement(symbol, **kwargs):
-        return AlphaVantageAPI._construct_url("INCOME_STATEMENT", \
-            symbol=symbol, **kwargs)
-    
+        return AlphaVantageAPI._construct_url(
+            "INCOME_STATEMENT", symbol=symbol, **kwargs
+        )
+
     @staticmethod
     def _construct_balance_sheet(symbol, **kwargs):
-        return AlphaVantageAPI._construct_url("BALANCE_SHEET", \
-            symbol=symbol, **kwargs)
-            
+        return AlphaVantageAPI._construct_url("BALANCE_SHEET", symbol=symbol, **kwargs)
+
     @staticmethod
     def _construct_cash_flow(symbol, **kwargs):
-        return AlphaVantageAPI._construct_url("CASH_FLOW", \
-            symbol=symbol, **kwargs)
+        return AlphaVantageAPI._construct_url("CASH_FLOW", symbol=symbol, **kwargs)
 
     @staticmethod
     def _construct_global_quote(symbol, **kwargs):
-        return AlphaVantageAPI._construct_url("GlOBAL_QUOTE", \
-            symbol=symbol, **kwargs)
-     
+        return AlphaVantageAPI._construct_url("GlOBAL_QUOTE", symbol=symbol, **kwargs)
+
     def get_intraday(self, symbol, interval, **kwargs):
         url = TimeSeries._construct_intraday(symbol, interval, **kwargs)
         return AlphaVantageAPI._get_query(url)
@@ -104,7 +112,7 @@ class TimeSeries:
     def get_cash_flow(self, symbol, **kwargs):
         url = TimeSeries._construct_cash_flow(symbol, **kwargs)
         return AlphaVantageAPI._get_query(url)
-    
+
     def get_quick_quote(self, symbol, **kwargs):
         url = TimeSeries._construct_global_quote(symbol, **kwargs)
         return AlphaVantageAPI._get_query(url)
@@ -115,25 +123,27 @@ class TimeSeries:
 if __name__ == "__main__":
     # Test the wrapper
     ts = TimeSeries()
-    #gme = ts.get_intraday("GME", "5min", outputsize="full")
-    #AlphaVantageAPI.save_json("GME", gme, label="intraday")
-    
-    #ibm = ts.get_intraday("IBM", "5min", outputsize="full")
-    #AlphaVantageAPI.save_json("IBM", ibm, label="intraday")
+    # gme = ts.get_intraday("GME", "5min", outputsize="full")
+    # AlphaVantageAPI.save_json("GME", gme, label="intraday")
 
-    #aeo = ts.get_intraday("AEO", "5min", outputsize="full")
-    #AlphaVantageAPI.save_json("AEO", aeo, label="intraday")
+    # ibm = ts.get_intraday("IBM", "5min", outputsize="full")
+    # AlphaVantageAPI.save_json("IBM", ibm, label="intraday")
+
+    # aeo = ts.get_intraday("AEO", "5min", outputsize="full")
+    # AlphaVantageAPI.save_json("AEO", aeo, label="intraday")
     url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=1PRBO66RYM7SV7B9&outputsize=full&datatype=csv"
     ibm = requests.get(url, proxies={}, headers={})
-    ibm = ibm.content.decode('utf-8')
-    cr = csv.reader(ibm.splitlines(), delimiter=',')
-    #print(list(cr))
-    #AlphaVantageAPI.save_json("IBM", ibm, label="daily_adjusted")
-    with open(f'./IBM_daily_adjusted_full{date.today().strftime("%Y%m%d")}.csv', 'w') as outfile:
+    ibm = ibm.content.decode("utf-8")
+    cr = csv.reader(ibm.splitlines(), delimiter=",")
+    # print(list(cr))
+    # AlphaVantageAPI.save_json("IBM", ibm, label="daily_adjusted")
+    with open(
+        f'./IBM_daily_adjusted_full{date.today().strftime("%Y%m%d")}.csv', "w"
+    ) as outfile:
         writer = csv.writer(outfile)
         for row in cr:
             writer.writerow(row)
-'''
+"""
     ibm = ts.get_quick_quote("IBM")
     AlphaVantageAPI.save_json("IBM", ibm, label="quick_quote")
 
@@ -148,16 +158,4 @@ if __name__ == "__main__":
 
     ibm = ts.get_cash_flow("IBM")
     AlphaVantageAPI.save_json("IBM", ibm, label="cash_flow")
-'''
-
-
-
-
-
-
-
-
-
-
-
-
+"""
--- model/lstm/run.py
+++ model/lstm/run.py
@@ -38,6 +38,7 @@ api = Api(
 PREDICTION_DAILY_NS = Namespace("model", "Prediction Queries and Results")
 api.add_namespace(PREDICTION_DAILY_NS)
 
+
 def prediction_daily_params_model(namespace):
     return namespace.model(
         "prediction_daily_params",
@@ -49,39 +50,42 @@ def prediction_daily_params_model(namespace):
         },
     )
 
+
 with open("./model.yaml", "r") as stream:
     config = yaml.safe_load(stream)
 
 models = {}
 models_info = config["models"]
 for k in models_info.keys():
-     filename = models_info[k]["save_filename"]
-     models[k] = tf.keras.models.load_model(filename)
+    filename = models_info[k]["save_filename"]
+    models[k] = tf.keras.models.load_model(filename)
 
 inf_modes = config["prediction_modes"]
 
-model_cnn_normaliser = MinMaxScaler(feature_range=(0,1))
+model_cnn_normaliser = MinMaxScaler(feature_range=(0, 1))
 normaliser_initial = np.zeros((60, 1))
-normaliser_initial[0,0] = 35.1228303438
-normaliser_initial[59,0] = 159.577350449
+normaliser_initial[0, 0] = 35.1228303438
+normaliser_initial[59, 0] = 159.577350449
 model_cnn_normaliser.fit(normaliser_initial)
+
 
 def featurise_multistep_series(inf_mode, initial_data, n_steps=60):
     """Transforms a (n_steps) shape array into a (n_steps, n_series, 1) shape array.
-        Args:
-            inf_mode (str): Inference mode (used to determine length as per data in inf_modes).
-            initial_data (numpy.ndarray): Initial data sequence ([d_0, d_1, d_2, ..., d_60]).
-            n_steps (int): number of steps in one set of data.
-        Return:
-            result (numpy.ndarray): Transformed data sequence.
-            e.g. ([[[d_0], [d_1], [d_2]], [[d_3], [d_4], [d_5]], [ ..., [d_60]]])
+    Args:
+        inf_mode (str): Inference mode (used to determine length as per data in inf_modes).
+        initial_data (numpy.ndarray): Initial data sequence ([d_0, d_1, d_2, ..., d_60]).
+        n_steps (int): number of steps in one set of data.
+    Return:
+        result (numpy.ndarray): Transformed data sequence.
+        e.g. ([[[d_0], [d_1], [d_2]], [[d_3], [d_4], [d_5]], [ ..., [d_60]]])
     """
     result = []
     for i in range(n_steps, inf_modes[inf_mode]["length"]):
-        result.append(initial_data[i-n_steps:i])
+        result.append(initial_data[i - n_steps : i])
     result = np.array(result)
     result = np.reshape(result, (result.shape[0], result.shape[1], 1))
     return result
+
 
 def featurise_single_series(x_input, n_entries=1, n_steps=60, n_features=1):
     """Transforms a (length) shape array into a (length, 1, 1) shape array.
@@ -96,6 +100,7 @@ def featurise_single_series(x_input, n_entries=1, n_steps=60, n_features=1):
     """
     return x_input.reshape((n_entries, n_steps, n_features))
 
+
 def featurise_cnn(x_input, n_entries=1, n_steps=60, n_seq=2, n_features=1):
     """Transforms a (length) shape array into a (length, n_sequences, n_steps_per_subsequence, n_features) shape array.
     Args:
@@ -108,8 +113,9 @@ def featurise_cnn(x_input, n_entries=1, n_steps=60, n_seq=2, n_features=1):
         result (numpy.ndarray): Transformed data sequence.
         e.g. ([[[d_0], [d_1], [d_2], ..., [d_30]], [[d_31], ..., [d_60]]])
     """
-    n_steps_cnn = int(n_steps/n_seq)
+    n_steps_cnn = int(n_steps / n_seq)
     return x_input.reshape((n_entries, n_seq, n_steps_cnn, n_features))
+
 
 def make_forecast(model, x_input):
     """Runs prediction on model given the x_input data sequence.
@@ -124,6 +130,7 @@ def make_forecast(model, x_input):
     y_pred = model.predict(x_input, verbose=0)
     return y_pred
 
+
 def get_walkforward_prediction(model, initial_data, n_steps=60, max_intervals=120):
     """Given an (n_steps) shape array, transform/featurise the last n_steps points, run the prediction, then pop the first point in the array and append the prediction. Continue until the number of desired predictions in made.
     Args:
@@ -136,18 +143,21 @@ def get_walkforward_prediction(model, initial_data, n_steps=60, max_intervals=12
     """
     predictions = []
     n_features = initial_data.shape[1] if len(initial_data.shape) >= 2 else 1
-    y_data = featurise_single_series(initial_data[-n_steps:], n_entries=1, n_steps=n_steps, n_features=1)
+    y_data = featurise_single_series(
+        initial_data[-n_steps:], n_entries=1, n_steps=n_steps, n_features=1
+    )
     for i in range(max_intervals):
         y_data = featurise_single_series(y_data, n_entries=1, n_steps=y_data.shape[1])
         y_pred = make_forecast(model, y_data)
         y_pred = y_pred[0]
         predictions.append(y_pred)
-        y_data = np.hstack(( y_data[:,1:,:], y_pred.reshape((1, 1, 1)) ))
+        y_data = np.hstack((y_data[:, 1:, :], y_pred.reshape((1, 1, 1))))
 
     predictions = np.array(predictions)
     predictions_c = np.concatenate(predictions)
-    predictions_c *= initial_data[-1:]/predictions_c[0]
+    predictions_c *= initial_data[-1:] / predictions_c[0]
     return predictions_c
+
 
 def get_walkforward_cnn(model, initial_data, n_steps=60, n_seq=2, max_intervals=120):
     """As per get_walkforward_prediction but apply the transform for data to be read by convolutional type networks.
@@ -162,16 +172,21 @@ def get_walkforward_cnn(model, initial_data, n_steps=60, n_seq=2, max_intervals=
     """
     initial_data = model_cnn_normaliser.transform(initial_data.reshape(-1, 1))
     predictions = []
-    x_data_i = featurise_single_series(initial_data[-n_steps:], n_entries=1, n_steps=n_steps, n_features=1)
+    x_data_i = featurise_single_series(
+        initial_data[-n_steps:], n_entries=1, n_steps=n_steps, n_features=1
+    )
     for i in range(max_intervals):
-        x_data = featurise_cnn(np.copy(x_data_i), n_entries=1, n_steps=n_steps, n_seq=n_seq, n_features=1)
+        x_data = featurise_cnn(
+            np.copy(x_data_i), n_entries=1, n_steps=n_steps, n_seq=n_seq, n_features=1
+        )
         y_pred = model.predict(x_data)
         predictions.append(y_pred)
-        x_data_i = np.hstack(( x_data_i[:,1:,:], y_pred.reshape((1, 1, 1)) ))
+        x_data_i = np.hstack((x_data_i[:, 1:, :], y_pred.reshape((1, 1, 1))))
     predictions = np.array(predictions)
     predictions_c = np.concatenate(predictions)
     predictions_c = model_cnn_normaliser.inverse_transform(predictions_c).reshape(-1)
     return predictions_c
+
 
 def get_multistep_vanilla(model, inf_mode, initial_data):
     """As per get_walkforward_prediction but apply a direct/minimal transform to the data (where the shape is known in advance).
@@ -187,66 +202,86 @@ def get_multistep_vanilla(model, inf_mode, initial_data):
     predictions = predictions.reshape((predictions.shape[0]))
     return predictions
 
+
 @app.route("/status")
 def index():
     return {"status": "ready"}
+
 
 # @app.route("/model/api/get_prediction", methods=["POST"])
 @PREDICTION_DAILY_NS.route("/api/get_prediction", methods=["POST"])
 class Retrieve_Prediction(Resource):
     @PREDICTION_DAILY_NS.doc(description="Run stock price prediction algorithm. ")
-    @PREDICTION_DAILY_NS.expect(prediction_daily_params_model(PREDICTION_DAILY_NS), validate=True)
+    @PREDICTION_DAILY_NS.expect(
+        prediction_daily_params_model(PREDICTION_DAILY_NS), validate=True
+    )
     @PREDICTION_DAILY_NS.response(200, "Successfully fetched prediction")
     @PREDICTION_DAILY_NS.response(404, "Prediction API Not Available")
     def post(self):
         result = {}
         try:
             request_data = request.get_json()
-            inf_mode = request_data['inference_mode']
-            initial_data = np.array(request_data['data'])
+            inf_mode = request_data["inference_mode"]
+            initial_data = np.array(request_data["data"])
             # Rescale data
-            normaliser_initial[0,0] = request_data['orig_data_min']
-            normaliser_initial[59,0] = request_data['orig_data_max']
+            normaliser_initial[0, 0] = request_data["orig_data_min"]
+            normaliser_initial[59, 0] = request_data["orig_data_max"]
             model_cnn_normaliser.fit(normaliser_initial)
 
             # This endpoint accepts 1-dimensional inputs and forms the n-step inputs/outputs internally
             if inf_mode not in inf_modes:
                 abort(500, f"Inference mode: {inf_mode} not supported")
             if len(initial_data.shape) > 1:
-                abort(500, f"Data received is {len(initial_data.shape[0])}-dimensional but data sequence must be 1-dimensional")
-            if not initial_data.dtype in [np.dtype('int'), np.dtype('float')]:
-                abort(500, f"Data received is of type {initial_data.dtype} but data sequence must be of type int or float")
+                abort(
+                    500,
+                    f"Data received is {len(initial_data.shape[0])}-dimensional but data sequence must be 1-dimensional",
+                )
+            if not initial_data.dtype in [np.dtype("int"), np.dtype("float")]:
+                abort(
+                    500,
+                    f"Data received is of type {initial_data.dtype} but data sequence must be of type int or float",
+                )
             data_length = initial_data.shape[0]
 
             result = {}
             # Select between inference modes and run prediction
             if inf_mode in inf_modes and data_length >= inf_modes[inf_mode]["length"]:
                 req_len = inf_modes[inf_mode]["length"]
-                initial_data = initial_data[-req_len:] if data_length > req_len else initial_data
+                initial_data = (
+                    initial_data[-req_len:] if data_length > req_len else initial_data
+                )
                 predictions = []
                 if inf_mode == "walk_forward":
-                    predictions = get_walkforward_prediction(models["lstm_vanilla"], initial_data)
+                    predictions = get_walkforward_prediction(
+                        models["lstm_vanilla"], initial_data
+                    )
                 elif inf_mode == "multistep_series":
-                    predictions = get_multistep_vanilla(models["lstm_vanilla"], inf_mode, initial_data)
+                    predictions = get_multistep_vanilla(
+                        models["lstm_vanilla"], inf_mode, initial_data
+                    )
                 elif inf_mode == "cnn":
                     predictions = get_walkforward_cnn(models["lstm_cnn"], initial_data)
 
                 predictions = predictions.tolist()
                 result = {"data": predictions}
             elif inf_mode in inf_modes and data_length < inf_modes[inf_mode]["length"]:
-                abort(500, f"Data length is {data_length} but required data length for mode \"{inf_modes[inf_mode]['name']}\" is {inf_modes[inf_mode]['length']}")
+                abort(
+                    500,
+                    f"Data length is {data_length} but required data length for mode \"{inf_modes[inf_mode]['name']}\" is {inf_modes[inf_mode]['length']}",
+                )
             else:
-                abort(500, f"Inference mode \"{inf_mode}\" not supported")
+                abort(500, f'Inference mode "{inf_mode}" not supported')
         except Exception as e:
             abort(500, e)
 
         return Response(dumps(result), status=200)
 
-if __name__ == "__main__":
-    if "ENVIRONMENT" in os.environ:
-        if os.environ["ENVIRONMENT"] == "production":
-            app.run(port=80, host="0.0.0.0")
-        elif os.environ["ENVIRONMENT"] == "local":
-            app.run(port=5000, host="0.0.0.0")
-    else:
-        app.run(port=5001, host="127.0.0.1")
+
+# if __name__ == "__main__":
+#     if "ENVIRONMENT" in os.environ:
+#         if os.environ["ENVIRONMENT"] == "production":
+#             app.run(port=80, host="0.0.0.0")
+#         elif os.environ["ENVIRONMENT"] == "local":
+#             app.run(port=5000, host="0.0.0.0")
+#     else:
+#         app.run(port=5001, host="127.0.0.1")
